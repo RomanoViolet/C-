9a33fc9715104a8b070608d00ea179c1adefc21d
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2019-12-03 10:55:43 +0000

    Added Test to Verify Failed Constructor.

---
+++ BoundedTypes/GoogleTests/NegativeTests/NegativeTests.cpp
@@ -0,0 +1,34 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include <Library/CustomTypes.hpp>
+#include <Library/SafeTypes.hpp>
+#include <limits>
+#include <type_traits>
+
+TEST( InstantationTest, CheckForZeroDenominator )
+{
+#if ( defined( __GNUC__ ) || defined( __clang__ ) )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
+
+  // long version
+  // auto f
+  //     = []( ) { return ( std::is_constructible< RomanoViolet::SafeType< 1, 0, 1, 1 > >::value );
+  //     };
+
+  // EXPECT_FALSE( f( ) );
+  EXPECT_FALSE( []( ) {
+    return ( std::is_constructible< RomanoViolet::SafeType< 1, 0, 1, 1 > >::value );
+  }( ) );
+
+  // const int MaxValueOfInt = 1;
+  // using customType = RomanoViolet::SafeType< Fraction( 1, 0 ), Fraction( MaxValueOfInt, 1 ) >;
+  // customType c = 0.9;  // actual value does not matter.
+#if ( defined( __clang__ ) || defined( __GNUC__ ) )
+#pragma GCC diagnostic pop
+#endif
+}
