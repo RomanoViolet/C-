62be2bc0a3cab6e254ea221c89cf7a47700a7397
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2021-08-29 20:26:08 +0000

    updates to header.hpp

--- FIFO/structured_bindings/header.hpp
+++ FIFO/structured_bindings/header.hpp
@@ -2,6 +2,9 @@
 #define HEADER_HPP_INCLUDED
 #include <cstdint>
 #include <type_traits>
+// https://akrzemi1.wordpress.com/2020/01/29/requires-expression/
+// https://akrzemi1.wordpress.com/2017/12/02/your-own-type-predicate/
+// https://godbolt.org/z/9z83P4a17
 template < typename T, uint8_t C > class CircularBuffer
 {
    public:
