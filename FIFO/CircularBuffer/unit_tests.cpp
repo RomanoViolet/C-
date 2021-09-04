e718725b9b65ae796bf2fc1ff51b8608d8a967ed
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2021-09-04 22:32:27 +0000

    updates

--- FIFO/CircularBuffer/unit_tests.cpp
+++ FIFO/CircularBuffer/unit_tests.cpp
@@ -8,6 +8,7 @@ TEST ( ShouldCompile, ZeroCapacityFIFO ) // NOLINT
     //CircularBuffer< int, 0U > f;
     static_assert ( ! NonZeroCapacityOfBuffer< int, 0 > );
     EXPECT_TRUE ( true );
+    // https://godbolt.org/z/5z19b74oz
     ASSERT_DEATH_IF_SUPPORTED (
         {
             CircularBuffer< int, 0 > b { };
