9cc3ec9f40ef61090ec3c48a8308d4ae0e8a6512
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2022-06-22 19:50:48 +0000

    pointers are not assigned correctly.

--- HelloComponents/CoreFunctions/Application/Consumer.cpp
+++ HelloComponents/CoreFunctions/Application/Consumer.cpp
@@ -1,11 +1,14 @@
 #include "Consumer.hpp"
-
+#include <iostream>
 void Consumer::initialize( )
 {
 }
 
 void Consumer::compute( )
 {
+  // read the value from producer and add 1 to it.
+  std::cout << "Received: " << this->a_in.getValue( ) << ". Is now: " << this->a_in.getValue( ) + 1
+            << std::endl;
 }
 
 void Consumer::doPreconditionCheck( )
@@ -22,3 +25,11 @@ void Consumer::doPostConditionCheck( )
 {
   // nothing till now.
 }
+
+#ifndef NDEBUG
+// debug mode
+int16_t Consumer::getStoredValue( )
+{
+  return this->a_in.getValue( );
+}
+#endif
